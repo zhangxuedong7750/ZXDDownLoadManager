9ec76b6c23452fe4dc583ca8fa5e86e3c15cbc7c
zhangxd7573 <zhangxuedong@zhangxuedongdeMacBook-Pro.local>
2016-02-02 20:51:22 +0800

    first commit

---
+++ ZXDDownLoadManager/ZXDSessionModel.h
@@ -0,0 +1,13 @@
+//
+//  ZXDSessionModel.h
+//  ZXDDownLoadManager
+//
+//  Created by 张雪东 on 16/1/17.
+//  Copyright © 2016年 ZXD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZXDSessionModel : NSObject
+
+@end
---
+++ ZXDDownLoadManager/ZXDSessionModel.m
@@ -0,0 +1,13 @@
+//
+//  ZXDSessionModel.m
+//  ZXDDownLoadManager
+//
+//  Created by 张雪东 on 16/1/17.
+//  Copyright © 2016年 ZXD. All rights reserved.
+//
+
+#import "ZXDSessionModel.h"
+
+@implementation ZXDSessionModel
+
+@end
